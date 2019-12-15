d90fc2d8ff8e6952f51064cd4897e1363f92645e
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2019-12-15 15:01:20 +0100

    AGR-020: Implement user profile editing screen.

--- app/src/main/java/sanchez/sanchez/sergio/brownie/extension/LongExt.kt
+++ app/src/main/java/sanchez/sanchez/sergio/brownie/extension/LongExt.kt
@@ -12,6 +12,35 @@ fun Long.toDateTimeString(appContext: Context): String{
     return SimpleDateFormat(appContext.getString(R.string.datetime_format)).format(calendar.time)
 }
 
+fun Long.toDateString(appContext: Context): String {
+    val calendar = Calendar.getInstance().also {
+        it.timeInMillis = this
+    }
+    return SimpleDateFormat(appContext.getString(R.string.date_format)).format(calendar.time)
+}
+
+fun Long.toDateStringWithAge(appContext: Context): String {
+    val calendar = Calendar.getInstance().also {
+        it.timeInMillis = this
+    }
+    return SimpleDateFormat(appContext.getString(R.string.date_format_with_age, getAge()))
+        .format(calendar.time)
+}
+
+fun Long.getAge(): Int {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this
+    val today = Calendar.getInstance()
+    var age = today.get(Calendar.YEAR) - calendar.get(Calendar.YEAR)
+    if (today.get(Calendar.MONTH) < calendar.get(Calendar.MONTH) || today.get(Calendar.MONTH) == calendar.get(
+            Calendar.MONTH
+        ) && today.get(Calendar.DAY_OF_MONTH) < calendar.get(Calendar.DAY_OF_MONTH)
+    ) {
+        age--
+    }
+    return age
+}
+
 fun Long.toDateTime(): Date{
     val calendar = Calendar.getInstance()
     calendar.timeInMillis = this
--- app/src/main/java/sanchez/sanchez/sergio/brownie/ui/core/fragment/SupportLCEFragment.kt
+++ app/src/main/java/sanchez/sanchez/sergio/brownie/ui/core/fragment/SupportLCEFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.annotation.CallSuper
-import androidx.core.view.ViewCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
