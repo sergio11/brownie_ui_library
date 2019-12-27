27676e0cc779fad785d20228fb4e2e09c7d98274
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2019-12-27 13:47:36 +0100

    AGR-026: Implement option to share post content.

--- app/src/main/java/sanchez/sanchez/sergio/brownie/extension/ActivityExt.kt
+++ app/src/main/java/sanchez/sanchez/sergio/brownie/extension/ActivityExt.kt
@@ -1,5 +1,7 @@
 package sanchez.sanchez.sergio.brownie.extension
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
@@ -22,12 +24,24 @@ fun SupportActivity.addFragment(containerViewId: Int, fragment: Fragment, addToB
     fragmentTransaction.commit()
 }
 
-
-
 fun <ViewT : View> AppCompatActivity.bindView(@IdRes idRes: Int): Lazy<ViewT> {
     return lazyUnsychronized {
         findViewById<ViewT>(idRes)
     }
+}
+
+/**
+ * Share Simple Content thought Android Sharesheet
+ * @param textTitle
+ * @param textContent
+ */
+fun Activity.shareSimpleContent(textTitle: String, textContent: String) {
+    startActivity(Intent.createChooser(Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TITLE, textTitle)
+        putExtra(Intent.EXTRA_TEXT, textContent)
+        type = "text/plain"
+    }, null))
 }
 
 
