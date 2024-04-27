fdbdb7a6d66624c645941d0b56dff28bd02ce925
Sergio Sánchez <sss4esob@gmail.com>
2024-04-27 13:33:08 +0200

    Implement BrownieTheme

---
+++ app/src/main/java/com/dreamsoftware/brownie/theme/Color.kt
@@ -0,0 +1,41 @@
+package com.dreamsoftware.brownie.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+val md_theme_light_primary = Color(0xFF6356e5)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF6356e5)
+val md_theme_light_onPrimaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_light_secondary = Color(0xFF50B9E0)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFF50B9E0)
+val md_theme_light_onSecondaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_light_tertiary = Color(0xFF845EC2)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFF845EC2)
+val md_theme_light_onTertiaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFFCFDF7)
+val md_theme_light_onBackground = Color(0xFF1A1C19)
+
+val md_theme_light_surface = Color(0xFFFCFDF7)
+val md_theme_light_onSurface = Color(0xFF1A1C19)
+val md_theme_light_surfaceVariant = Color(0xFF6356e5)
+val md_theme_light_onSurfaceVariant = Color(0xFFDDE5DA)
+
+val md_theme_light_outline = Color(0xFF727970)
+val md_theme_light_inverseOnSurface = Color(0xFFF0F1EC)
+val md_theme_light_inverseSurface = Color(0xFF2E312E)
+val md_theme_light_inversePrimary = Color(0xFF86D992)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF166D33)
+val md_theme_light_outlineVariant = Color(0xFFC1C9BE)
+val md_theme_light_scrim = Color(0xFF000000)
---
+++ app/src/main/java/com/dreamsoftware/brownie/theme/ColorScheme.kt
@@ -0,0 +1,33 @@
+package com.dreamsoftware.brownie.theme
+
+import androidx.compose.material3.lightColorScheme
+
+val BrownieLightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
+    surfaceTint = md_theme_light_surfaceTint,
+    scrim = md_theme_light_scrim
+)
