0e3b97afca0bc190e7579a152a0cae39db104fae
Sergio Sánchez <sss4esob@gmail.com>
2024-05-06 19:03:28 +0200

    Implement BrownieCardColumn

---
+++ app/src/main/java/com/dreamsoftware/brownie/component/BrownieCardColumn.kt
@@ -0,0 +1,35 @@
+package com.dreamsoftware.brownie.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BrownieCardColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Card(
+        modifier = Modifier.padding(20.dp).then(modifier),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(Color.White.copy(alpha = 0.7f)),
+        shape = RoundedCornerShape(27.dp),
+        border = BorderStroke(3.dp, Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 20.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            content = content
+        )
+    }
+}
