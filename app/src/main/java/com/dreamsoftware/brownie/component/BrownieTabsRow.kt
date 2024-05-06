317ba244b91a07cc88977d68d261e64e77128f1e
Sergio Sánchez <sss4esob@gmail.com>
2024-05-06 20:10:53 +0200

    Move BrownieTabUI model to BrownieTabsRow file

--- app/src/main/java/com/dreamsoftware/brownie/component/BrownieTabsRow.kt
+++ app/src/main/java/com/dreamsoftware/brownie/component/BrownieTabsRow.kt
@@ -1,5 +1,7 @@
 package com.dreamsoftware.brownie.component
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Tab
@@ -10,8 +12,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.dreamsoftware.brownie.core.BrownieTabUi
 import com.dreamsoftware.brownie.utils.tabSelectedIndex
+
+data class BrownieTabUi<out T>(
+    @DrawableRes val iconRes: Int,
+    @StringRes val titleRes: Int,
+    var isSelected: Boolean = false,
+    val type: T
+)
 
 @Composable
 fun <T>BrownieTabsRow(
