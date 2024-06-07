a7a0bf075115fb165ae0ba7b31b7d415348586b8
Sergio Sánchez <sss4esob@gmail.com>
2024-06-07 20:44:27 +0200

    Update to version 0.0.18

---
+++ app/src/main/java/com/dreamsoftware/brownie/component/BrownieSheetSurface.kt
@@ -0,0 +1,48 @@
+package com.dreamsoftware.brownie.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BrownieSheetSurface(
+    surfaceColor: Color = Color.White,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Center,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    onSurfaceContent: @Composable ColumnScope.() -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+        color = surfaceColor
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .background(color = surfaceColor),
+            verticalArrangement = verticalArrangement,
+            horizontalAlignment = horizontalAlignment
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            onSurfaceContent()
+        }
+    }
+}
